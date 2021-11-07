1b6fc9c5847d499377a6279f866f566576986732
Rghifari <93661771+Rghifari@users.noreply.github.com>
2021-11-07 22:00:36 +0700

    labspy02

---
+++ tugas.py
@@ -0,0 +1,11 @@
+print ('menentukan bilangan terbesar')
+print ('menentukan 3 bilangan yang diinginkan')
+a = int (input ('bilangan pertama ='))
+b = int (input ('bilangan kedua ='))
+c = int (input ('bilangan ketiga ='))
+if a>b and a>c :
+    print ('bilangan terbesar =',a)
+elif b>a and b>c :
+    print ('bilangan terbesar =',b)
+else :
+    print (c,'bilangan terbesar =',c)
